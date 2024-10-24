d1fb3ed77e8bdf47715f8bf682438c61afb8d5f1
aulialili602 <aulialili602@gmail.com>
2024-10-24 14:12:15 +0700

    selesai

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
 
+  let tinggiBadan = [150, 155, 148, 165, 170, 168, 166]
+  let namaAtlet = ["Rizki", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
+  
+  for(let i =0; i <namaAtlet.length; i++) {
+     console.log(`Atlet ${namaAtlet[i]} Tinggi Badan ${tinggiBadan[i]} cm`)
+  }
+
 
 
 
@@ -9,5 +16,6 @@ function runner() {
 
   /// JANGAN UBAH SELAINNYA
 }
+runner()
 
 module.exports = runner
